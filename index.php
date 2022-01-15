dbd0a4883ff52b8f7307c649aac179aa1029ceba
Michael Spencer <michael.spencer1@g.austincc.edu>
2022-01-14 18:21:35 -0600

    initial commit, html layed out

---
+++ index.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Michael Spencer | Web Solutions</title>
+    <link href="css/index.css" rel="stylesheet">
+</head>
+<body>
+<section>
+    <nav>
+        <ul>
+            <li>
+                <button>Recent work</button>
+                <button>I need a website</button>
+            </li>
+            <li>
+                <ul>
+                    <li><a href="#">About</a></li>
+                    <li><a href="#">Recent Work</a></li>
+                    <li><a href="#">Get In Touch</a></li>
+                </ul>
+            </li>
+        </ul>
+    </nav>
+    <div class="hero"></div>
+    <div class="overlay"></div>
+    <div class="header-content">
+        <h1>Michael Spencer</h1>
+        <p>Beautifully Created Web Experiences</p>
+        <div class="cta-container">
+            <button>Recent work</button>
+            <button>Web Solution</button>
+        </div>
+    </div>
+</section>
+<section>
+    <h2>About Me</h2>
+    <p>My name is Michael Spencer, I am a freelance web developer specialized in creating user friendly websites and user experiences that look great on any desktop or device.</p>
+    <div class="techs-container">
+        <div class="tech">
+            <img src="assets/js-square.svg" alt="" class="tech-icon">
+        </div>
+        <div class="tech">
+            <img src="assets/html5.svg" alt="" class="tech-icon">
+        </div>
+        <div class="tech">
+            <img src="assets/css3-alt.svg" alt="" class="tech-icon">
+        </div>
+        <div class="tech">
+            <img src="assets/mobile-alt.svg" alt="" class="tech-icon">
+        </div>
+    </div>
+</section>
+<section>
+    <h2>What I Can Do</h2>
+    <p>Creating an aesthetically pleasing and easy user experience.</p>
+    <div class="wicd-tri-container">
+        <div class="wicd">
+            <icon class="wicd-icon"></icon>
+            <h3>UX/UI Design</h3>
+            <p>Imagine, wireframe, sketch, and create simple, aesthetic, user-friendly, and fully functional user interfaces.</p>
+        </div>
+        <div class="wicd">
+            <icon class="wicd-icon"></icon>
+            <h3>Business Solutions</h3>
+            <p>I'll take care of "how it is going to look attractive" and "how it is going to work effectively.</p>
+        </div>
+        <div class="wicd">
+            <icon class="wicd-icon"></icon>
+            <h3>Build and Deploy</h3>
+            <p>HTML(5), CSS(3), Javascript(ES6), and related tools, libraries, frameworks.</p>
+        </div>
+    </div>
+</section>
+<section>
+    <h3>Recent Work</h3>
+    <p>Web Mock-ups and javascript apps</p>
+    <div class="works-container">
+        <div class="work">
+            <div class="work-overlay c1"></div>
+            <h4>Simon</h4>
+            <img src="assets/w2.png" alt="" class="work-img">
+        </div>
+        <div class="work">
+            <div class="work-overlay c2"></div>
+            <h4>2048</h4>
+            <img src="assets/w3.png" alt="" class="work-img">
+        </div>
+        <div class="work">
+            <div class="work-overlay c3"></div>
+            <h4>Moove-it Mock</h4>
+            <img src="assets/w4.png" alt="" class="work-img">
+        </div>
+        <div class="work">
+            <div class="work-overlay c4"></div>
+            <h4>Twitch Viewer</h4>
+            <img src="assets/w5.png" alt="" class="work-img">
+        </div>
+        <div class="work">
+            <div class="work-overlay c5"></div>
+            <h4>Pomodoro Clock</h4>
+            <img src="assets/w6.png" alt="" class="work-img">
+        </div>
+        <div class="work">
+            <div class="work-overlay c6"></div>
+            <h4>Real Estate</h4>
+            <img src="assets/w7.png" alt="" class="work-img">
+        </div>
+    </div>
+</section>
+<section>
+    <h3>Get In Touch</h3>
+    <p>Contact me for help in web solutions.</p>
+    <div class="contact-container">
+        <form action="" class="contact-form">
+            <div>
+                <label for="name">Name</label>
+                <input id="name" type="text" name="name">
+            </div>
+            <div>
+                <label for="number">Number</label>
+                <input id="number" type="text" name="number">
+            </div>
+            <div>
+                <label for="email">Email</label>
+                <input id="email" type="text" name="email">
+            </div>
+            <div>
+                <label for="Message">Message</label>
+                <textarea id="message" type="text" name="message"></textarea>
+            </div>
+            <input type="submit" value="Inquire">
+        </form>
+        <iframe width="100%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=200%20millard%20st%20georgetown%20tx%2078628+(Your%20Business%20Name)&amp;t=k&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+    </div>
+</section>
+<footer>
+    <p>Website designed and created by Michael 2022©</p>
+</footer>
+<script src="js/index.js"></script>
+</body>
+</html>
